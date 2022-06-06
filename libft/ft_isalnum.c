0ea0b1478b6aea35199c7182d61c9094034e7e43
haward <wndddms12345@naver.com>
2022-06-06 18:09:42 +0900

    처음으로 만들었는데 이게 제대로 만든건지도 모르겠고 그냥 만들어봄...

---
+++ libft/ft_isalnum.c
@@ -0,0 +1,4 @@
+int ft_isalnum(int c)
+{
+	return (('0' <= c && c <= '9') || ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z'));
+}
---
+++ libft/ft_isascii.c
@@ -0,0 +1,4 @@
+int ft_ascii(int c)
+{
+	return (0 <= c && c <= 127);
+}
