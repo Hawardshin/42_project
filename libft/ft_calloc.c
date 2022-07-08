14af6e5d809aea2cdabcf187311fed04cebf4345
wnddms12345 <wnddms12345@naver.com>
2022-07-08 14:42:49 +0900

    this is my part 1

---
+++ libft/ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joushin <joushin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 13:47:02 by joushin           #+#    #+#             */
+/*   Updated: 2022/07/08 14:34:28 by joushin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*result;
+
+	result = (void *)malloc(count * size);
+	if (!result)
+		return (0);
+	ft_bzero(result, count * size);
+	return (result);
+}
