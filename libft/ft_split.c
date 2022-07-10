fec827eaa3637d5f0340f050d997f3690b299a9c
haward <Hawardshin@naver.com>
2022-07-10 12:41:31 +0900

    strjoin split strtrim in home

---
+++ libft/ft_split.c
@@ -0,0 +1,75 @@
+
+#include"libft.h"
+
+
+static int	len_count(char *str, char c)
+{
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while (str[i] && str[i] == c)
+		i++;
+	while (str[i])
+	{
+		while (str[i] && str[i] == c)
+			i++;
+		count++;
+		while (str[i] && str[i] != c)
+			i++;
+	}
+	return (count);
+}
+
+static int	len_into(char *str, char c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && str[i] != c)
+		i++;
+	return (i);
+}
+
+static void	all_free(int i, char **result)
+{
+	while (i >= 0)
+	{
+		free(result[i]);
+		i--;
+	}
+	free(result);
+}
+
+char	**ft_split(char const *str, char c)
+{
+	char	**result;
+	int		i;
+	int		k;
+
+	i = 0;
+	result = (char **)malloc(sizeof(char *) * len_count(str, c) + 1);
+	if (!result)
+		return (0);
+	while (*str && *str == c)
+		str++;
+	while (*str)
+	{
+		k = 0;
+		result[i] = malloc(sizeof(char) * (len_into(str, c) + 1));
+		if (!result[i])
+		{
+			all_free(i,result);
+			return (0);
+		}
+		while (*str && *str != c)
+			result[i][k++] = *str++;
+		result[i][k] = '\0';
+		i++;
+		while (*str && *str == c)
+			str++;
+	}
+	result[i] = 0;
+	return (result);
+}
---
+++ libft/ft_strjoin.c
@@ -0,0 +1,29 @@
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char *const *s2)
+{
+	size_t 	len1;
+	size_t	len2;
+	char	*retstr;
+	size_t 	i;
+
+	i = 0;
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	retstr = (char *)malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!retstr)
+		return (0);
+	while (i < len1)
+	{
+		retstr[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (i < len2)
+	{
+		retstr[len1 + i] = s2[i];
+		i++;
+	}
+	retstr[len1 +len2] = '\0';
+	return (retstr);
+}
