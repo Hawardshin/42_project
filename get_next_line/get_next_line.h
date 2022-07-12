8ef9d66739f7bd0ecdbd11d8520b83dc4173022f
wnddms12345 <wnddms12345@naver.com>
2022-07-12 18:29:18 +0900

    get_next_line start

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joushin <joushin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/12 18:19:56 by joushin           #+#    #+#             */
+/*   Updated: 2022/07/12 18:26:39 by joushin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+#endif
