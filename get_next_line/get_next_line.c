2e74a2a18b913c4d42bf96c562a9eaf9dfd4709c
wnddms12345 <wnddms12345@naver.com>
2022-07-17 10:53:30 +0900

    gnl testing...

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: joushin <joushin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 18:19:08 by joushin           #+#    #+#             */
-/*   Updated: 2022/07/16 17:49:43 by joushin          ###   ########.fr       */
+/*   Updated: 2022/07/17 10:47:22 by joushin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,15 +30,20 @@ int	check_new_line(char *buf)
 char	*ft_read_line(int fd, char *backup)
 {
 	char	buff[BUFFER_SIZE + 1];
-	size_t	read_size;
+	ssize_t	read_size;
 	char	*to_read;
 
 	to_read = backup;
 	while (!check_new_line(buff))
 	{
 		read_size = read(fd, buff, BUFFER_SIZE);
-		if (read_size < 0)
+		// printf("%zu\n",read_size);
+		if (read_size < 0 || !buff[0])
+		{
+			if (backup)
+				free(backup);
 			return (NULL);
+		}
 		buff[read_size] = '\0';
 		if (read_size == 0)
 			return (to_read);
@@ -52,94 +57,89 @@ char	*ft_read_line(int fd, char *backup)
 
 char	*ft_get_line(char **str)
 {
-	 char	*ret_line;
-	 int		l;
+	char	*ret_line;
+	int		l;
 	int		i;
+	int		slen;
 
+	slen = ft_strlen(*str);
 	i = 0;
 	l = check_new_line(*str);
-	 if (l == -1)
-	 	return (*str);
-	 else
-	 {
-	 	ret_line = (char *)malloc(sizeof(char) * (l +1));
-	 	if (!ret_line)
-	 		return (NULL);
-	 	while (i < l)
-	 	{
-	 		ret_line[i] = *(*str + i);
-	 		i++;
-	 	}
-	 	ret_line[i] = '\0';
-	 }//여기 까지 봤어요.
-	 ft_memmove(*str, *str + l, ft_strlen(*str) - l);
-	 (*str)[l] = '\0';
-	 return (ret_line);
+	//printf("ll:%s\n",*str);
+	if (l == 0)//이건 아에 끝날떄 코드이다.
+	{
+		//printf("ll:%s\n",*str);
+		ret_line = ft_strdup(*str);
+		free(*str);
+		*str = NULL;
+		return (ret_line);
+	}
+	else
+	{
+		ret_line = (char *)malloc(sizeof(char) * (l +1));
+		if (!ret_line)
+			return (NULL);
+		while (i < l)
+		{
+			ret_line[i] = *(*str + i);
+			i++;
+		}
+		ret_line[i] = '\0';
+	}
+	ft_memmove(*str, *str + l, slen - l);
+	(*str)[slen - l] = '\0';
+	return (ret_line);
 }
 
 char	*get_next_line(int fd)
 {
-	int			move;
 	static char	*backup;
 	char		*ret_line;
-	char		*tmp;
 	int			i;
 
 	i = 0;
-	move = 0;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	tmp = ft_read_line(fd, backup);
-	//printf("%s\n",tmp);
-	if (tmp && !backup)
-	{
-		backup = tmp;
-		backup[ft_strlen(tmp)] = '\0';
-	}
-	else if (!tmp)
-	{
-		free(backup);
-		backup = tmp;
-		//backup[ft_strlen(tmp)] = '\0';
-	}
+	backup = ft_read_line(fd, backup);
+	//printf("aaa%s\n",backup);
+	if (!backup)
+		return (NULL);
 	//printf("%s\n",backup);
 	ret_line = ft_get_line(&backup);
-	move = check_new_line(backup);
 	if (ret_line == NULL)
 		return (NULL);
-	while (i < move)
-	{
-		backup++;
-		i++;
-	}
 	return (ret_line);
 }
 
-#include <fcntl.h>
-#include <stdio.h>
+// #include <fcntl.h>
+// #include <stdio.h>
 
-int	main(void)
-{
-  int fd;
-  //atest.txt 를 읽음
-  fd = open("./atest.txt", O_RDONLY);
-  char *result ;
-  for (int i = 0; i < 5; i++)
-  {
-	  result = get_next_line(fd);
-	  if (!result)
-	  {
-			printf("malloc no\n");
-			return (0);
-	  }
-	  printf("\n------aaaa------\n");
-	  printf("%s", result);
-	//   printf("%p\n",result);
-	  free(result);
-	  printf("-----nnnn----\n");
-  }
-  return (0);
-}
-/*read를 실패한다.
-*/
-//
+// int	main(void)
+// {
+//   int fd;
+//   //atest.txt 를 읽음
+//   //fd = open("./atest.txt", O_RDONLY);
+//   //fd= open("./1chartest.txt",O_RDONLY);
+//  fd= open("./newline.txt",O_RDONLY);
+//   char *result ;
+//   int i =0;
+//   while (1)
+//   {
+// 	  result = get_next_line(fd);
+// 	  if (!result)
+// 	  {
+// 			printf("malloc no\n");
+// 			return (0);
+// 	  }
+// 	  printf("\n------first:%d------\n",i);
+// 	  printf("%s", result);
+// 	//   printf("%p\n",result);
+// 	  //free(result);
+// 	  printf("-----end : %d----\n",i);
+// 	  i++;
+//   }
+//   return (0);
+// }
+// /*read를 실패한다.
+// */
+// //
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: joushin <joushin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/12 18:19:56 by joushin           #+#    #+#             */
-/*   Updated: 2022/07/16 17:24:40 by joushin          ###   ########.fr       */
+/*   Updated: 2022/07/17 08:45:26 by joushin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
@@ -21,8 +25,7 @@ char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strdup(const char *s1);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 size_t	ft_strlen(const char *str);
-// #ifndef	BUFFER_SIZE
-// # define BUFFER_SIZE 42
+
 // # elif BUFFER_SIZE < 0
 // #error
 
