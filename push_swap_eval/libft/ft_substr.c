49470b0a4b9df6108e38487155d47731a34adfb1
wnddms12345 <wnddms12345@naver.com>
2022-08-16 19:20:15 +0900

    for eval

---
+++ push_swap_eval/libft/ft_substr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joushin <joushin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 18:14:25 by joushin           #+#    #+#             */
+/*   Updated: 2022/07/11 12:07:52 by joushin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	int		i;
+	size_t	slen;
+
+	i = 0;
+	if (!s)
+		return (0);
+	slen = ft_strlen(s);
+	if (start >= slen)
+	{
+		str = (char *) malloc(sizeof(char));
+		str[0] = 0;
+		return (str);
+	}
+	if (slen - start < len)
+		len = slen - start;
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (0);
+	ft_strlcpy(str, s + start, len + 1);
+	return (str);
+}
