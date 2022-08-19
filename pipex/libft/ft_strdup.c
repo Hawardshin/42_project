100b6310a2b37f86a9f7d51f29feba81f9852480
wnddms12345 <wnddms12345@naver.com>
2022-08-19 15:56:22 +0900

    pipex start

---
+++ pipex/libft/ft_strdup.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joushin <joushin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/08 14:36:35 by joushin           #+#    #+#             */
+/*   Updated: 2022/07/11 08:13:57 by joushin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	char	*result;
+	size_t	len;
+
+	len = ft_strlen(s1);
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (!result)
+		return (0);
+	ft_strlcpy(result, s1, len + 1);
+	return (result);
+}
---
+++ pipex/pipex.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joushin <joushin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/19 15:07:21 by joushin           #+#    #+#             */
+/*   Updated: 2022/08/19 15:37:46 by joushin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <fcntl.h>  //open
+# include <unistd.h> //close, read, write, dup, dup2, exeve, fork
+# include <stdlib.h> //malloc, free
+# include <stdio.h>  //perror
+# include <string.h> //strerror
+
+#endif
