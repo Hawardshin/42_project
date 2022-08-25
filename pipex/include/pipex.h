cacf4cfe743726ecf09b9e059a84014601a299d1
wnddms12345 <wnddms12345@naver.com>
2022-08-25 16:57:48 +0900

    pipex 수정중이고 현재 cmd_init수정중 아직 rare한 상태

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: joushin <joushin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 15:07:21 by joushin           #+#    #+#             */
-/*   Updated: 2022/08/24 20:38:24 by joushin          ###   ########.fr       */
+/*   Updated: 2022/08/25 16:57:13 by joushin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,26 +23,33 @@
 
 typedef struct s_px
 {
-	char		**cmd[3];// 명령 모음
-	char		**path;// 경로
-	char		*cmd_path[3][2];//명령어 경로 모음
-	int			pipefd[2];// 파이프 fd 0이 출구 1이 입구
-	// int			num;//??
-	char		*infile;//입력 파일
-	char		*outfile;// 출력 파일
-	char		**ev;// 환 경변수 모음
-	// char		**exe_path;//명령어 실행 경로
-	struct s_px	*prev;//이전
-	struct s_px	*next;//다음
+	char	**cmd; //명령어 , 옵션들, 널
+	char	*cmd_path[2]; //명령어 경로 합쳐서, 널//둘다 널로 초기화 해주기
+	int		idx;// 몇번째 파이프인지
+	struct s_px *bef;
+	struct s_px	*next;
 }t_px ;
+
+typedef struct s_data
+{
+	char	*infile;//들어온 파일
+	char	*outfile;//나갈 파일
+	char	**ev;//환경변수들
+	char	**path;//환경변수에서 자른 path
+	int		pipefd[2];//처음에 만든 파이프
+	int		pipe_num;
+	t_px	*cmd_node_head;//각 명령어 별로 노드로
+	t_px	*cmd_node_tail;
+}t_data;
 
 void	ft_error_check(int idx, t_px *pipex);
 void	my_free(char **to_free);
 char	*mft_strdup(const char *s1);
-void	parse_input(t_px *pipex, char **argv, char **envp);
+void	parse_input(t_data *px, char **argv, char **envp);
 void	check_cmd(t_px *pipex);
 void	level_fuc(t_px *pipex);
-
+void	print_error(int	Flag, char *s);
+char	**ft_msplit(char const *str, char c);
 #endif
 // fork할 때 pid가 0 : 자식이다.
 // pipe함수
--- pipex/srcs/main.c
+++ pipex/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: joushin <joushin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/19 15:06:23 by joushin           #+#    #+#             */
-/*   Updated: 2022/08/24 18:51:17 by joushin          ###   ########.fr       */
+/*   Updated: 2022/08/25 16:57:11 by joushin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,52 +28,121 @@ char	*mft_strdup(const char *s1)
 	len = ft_strlen(s1);
 	result = (char *)malloc(sizeof(char) * (len + 1));
 	if (!result)
-		exit(1);
+		print_error(0,NULL);
 	ft_strlcpy(result, s1, len + 1);
 	return (result);
 }
 
-void	parse_input(t_px *pipex, char **argv, char **envp)
+void	print_error(int	Flag, char *s)
+{
+	if (Flag == 0)
+	{
+		ft_putstr_fd("Memory allocation fails.\n", 2);
+		exit(1);
+	}
+	else if (Flag == 1)
+	{
+		ft_eprintf("command not found: %s\n", s);
+		exit(127);
+	}
+	else if (Flag == 2)
+	{
+		perror(s);
+		exit(1);
+	}
+	else if (Flag == 3)
+	{
+		ft_eprintf("no such file or directory: %s\n",s);
+		exit(1);
+	}
+}
+
+void	node_init(t_data *px, char **argv)
+{
+	int		i;
+	t_px	*node;
+
+	i = -1;
+	while (++i < px->pipe_num)
+	{
+		node = (t_px *)malloc(sizeof (t_px));
+		if (!node)
+			print_error(0,NULL);
+		node->next = NULL;
+		node->idx = i;
+		node ->cmd = ft_msplit(argv[i + 2],' ');//argv[0] : 실행파일 argv[1] : 파일1번
+		if (px->cmd_node_head != NULL)
+		{
+			node->bef = px->cmd_node_tail;
+			px->cmd_node_tail->next = node;
+		}
+		else
+		{
+			px->cmd_node_head = node;
+			node->bef = NULL;
+		}
+		px->cmd_node_tail = node;
+	}
+}
+
+void	parse_input(t_data *px, char **argv, char **envp)
 {
 	char	*tmp_path;
 
-	pipex->infile = mft_strdup(argv[1]);
-	pipex->cmd[0] = ft_split(argv[2], ' ');//char ** cmd[3];
-	pipex->cmd[1] = ft_split(argv[3], ' ');
-	pipex->outfile = mft_strdup(argv[4]);
-	pipex->cmd[2] = 0;//필요?
-	while (ft_strncmp(*envp,"PATH",4) != 0 && envp)
+	px->infile = mft_strdup(argv[1]);
+	px->outfile = mft_strdup(argv[4]);
+	px->ev = mft_strdup(envp);
+	//if argc 갯수가 5일 때
+	px->pipe_num = 2;
+	node_init(px, argv);
+	while (ft_strncmp(*envp, "PATH", 4) != 0 && envp)
 		envp++;
-	tmp_path = mft_strdup(*envp + 5);
-	pipex -> path = ft_split(tmp_path, ':');
-	my_free(&tmp_path);
+	if (!envp)
+		print_error(2, "Not exist path!\n");
+	px -> path = ft_msplit(mft_strdup(*envp + 5), ':');
+	if (pipe(px->pipefd) == -1)
+		print_error(2, "pipe error\n");
 }
+t_px	*mlst_find(int idx, t_data *data)
+{
+	t_px	*node;
 
+	node = data->cmd_node_head;
+	while (node)
+	{
+		if (node->idx == idx)
+			return (node);
+		node = node ->next;
+	}
+	return (NULL);
+}
 //만약 경로상에 존재하지 않는다면 현재 디렉토리에서도 탐색하는걸 추가할까?
-void	check_cmd(t_px *pipex)
+void	cmd_init(t_data *px)
 {
 	char	*pathname;
 	int		idx;
 	int		i;
 	char	*cmdtmp;
+	t_px	*node;
 
 	idx = -1;
 	while (++idx < 2)//맨대토리라 명령어 2개
 	{
-		cmdtmp = ft_strjoin("/",pipex->cmd[idx][0]);
+		node = mlst_find(idx, px);
+		node ->cmd_path[0] = 0;
+		node ->cmd_path[1] = 0;
+		cmdtmp = ft_strjoin("/",node->cmd[0]);
 		// my_free(pipex->cmd[idx][0]);//이건 나중에 프리해주는걸로 하자.
 		i = -1;
-		while (pipex -> path[++i])
+		while (px -> path[++i])
 		{
-			pathname = ft_strjoin(pipex-> path[i], cmdtmp);
+			pathname = ft_strjoin(px-> path[i], cmdtmp);
 			if (!pathname)
-				return ;
+				print_error(0, NULL);
 			if (access(pathname, R_OK | X_OK) == 0)
-				pipex->cmd_path[idx][0] = mft_strdup(pathname);
+				node->cmd_path[0] = mft_strdup(pathname);
 			my_free(&pathname);
 		}
-		if(!(pipex->cmd_path[idx][0]))//
-			pipex->cmd_path[idx][0] = NULL;
 		// printf("ak: %s\n",pipex->cmd_path[idx][0]);
 		my_free(&cmdtmp);
 	}
@@ -98,21 +167,8 @@ void	level_fuc(t_px *pipex)
 	}//자식 프로세스
 	else if (pid == 0)
 	{
-		fds = open(pipex->infile, O_RDONLY);
-		if (fds == -1)
-		{
-			perror("open error : ");
-			exit(1);
-		}
-		if (dup2(fds, 0) == -1)
-			exit(1);
-		if (dup2(pipex->pipefd[1], 1) == -1)
-			exit(1);
-		close(fds);
-		close(pipex->pipefd[1]);
-		ft_error_check(idx, pipex);
-		if (pipex->cmd_path[idx][0] != NULL)
-			execve(pipex->cmd_path[idx][0], (pipex->cmd[idx]), pipex->ev);
+
+
 	}
 	else
 	{//부모프로세스
@@ -134,19 +190,66 @@ void	level_fuc(t_px *pipex)
 		ft_error_check(idx, pipex);
 		if (pipex->cmd_path[idx][0] != NULL)
 			execve(pipex->cmd_path[idx][0], (pipex->cmd[idx]), pipex->ev);
-
 	}
 }
 
+pid_t	mfork(void)//fork를 실패하면 프로세스를 종료하는 게 맞는걸까?
+{
+	pid_t	rpid;
+
+	rpid = fork();
+	if (rpid == -1)
+	{
+		perror("fork error ");
+		exit(1);
+	}
+	return (rpid);
+}
+
+void	exec_child(int idx, t_data *px)
+{
+	int	o_fd;
+
+	if (idx == 0)
+	{
+		o_fd = open(px->infile, O_RDONLY);
+		if (o_fd == -1)
+			print_error(3, px->infile);
+		if (dup2(o_fd, 0) == -1)
+			print_error(2, NULL);
+		if (dup2(px->pipefd[1], 1) == -1)
+			print_error(2, NULL);
+		close(o_fd);
+		close(px->pipefd[1]);
+	}
+	if (px->cmd_path[idx][0] != NULL)
+		execve(pipex->cmd_path[idx][0], (pipex->cmd[idx]), pipex->ev);
+}
+
+void	fork_child(t_data *px)
+{
+	int		i;
+	pid_t	pid;
+
+	i = -1;
+	while (++i < px->pipe_num)
+	{
+		pid = mfork();
+		if (pid == 0)
+			exec_child(i, px);
+	}
+}
 int	main(int argc, char **argv, char **envp)
 {
-	t_px	pipex;
+	t_data	px;
 	if (argc == 5)
 	{
-		ft_memset(&pipex, 0, sizeof(t_px));
-		pipex.ev = envp;
-		parse_input(&pipex, argv, envp);//파싱하기
-		check_cmd(&pipex);//명령어 체크해서 있으면 제대로 없으면 NULL로 채워주기]
+		ft_memset(&px, 0, sizeof(t_px));
+		px.ev = envp;
+		parse_input(&px, argv, envp);//파싱하기
+		cmd_init(&px);//명령어 체크해서 있으면 제대로 없으면 NULL로 채워주기]
+		fork_child(&px);
+
 		if (pipe(pipex.pipefd) == -1)
 		{
 			perror("pipe error ");
