46f2eff9a98c9f7af6a2c80180871c6e168c0e28
wnddms12345 <wnddms12345@naver.com>
2022-11-16 19:51:58 +0900

    토큰화 완료했어요 함수로 다 쪼갰습니다. 이제 파일로 쪼갤께요

--- REAL_MINISHELL/token.c
+++ REAL_MINISHELL/token.c
@@ -6,7 +6,7 @@
 /*   By: joushin <joushin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 17:14:42 by joushin           #+#    #+#             */
-/*   Updated: 2022/11/16 19:16:59 by joushin          ###   ########.fr       */
+/*   Updated: 2022/11/16 19:51:13 by joushin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,7 +77,7 @@ int	token_case(char a)
 	return (CHAR);
 }
 
-int	ft_token_len(t_readline *src)
+int	ft_quote_len(t_readline *src)
 {
 	int		len;
 	int		i;
@@ -106,7 +106,7 @@ t_token	*create_quotes(t_readline *src)
 	i = 0;
 	move_char(src);
 	tok = malloc(sizeof(t_token));
-	len = ft_token_len(src);
+	len = ft_quote_len(src);
 	tok->text = malloc(sizeof(char) * (len + 1));
 	tok->text_len = len;
 	while (token_case(see_char(src)) != QUOTES && see_char(src) != ENDOF)
@@ -298,65 +298,98 @@ t_token	*create_redirect_tok(t_readline *src)
 	return (tok);
 }
 
-// t_token	*create_dollar_tok(t_readline *src)
-// {
-// 	t_token	*tok;
+int	ft_dollar_len(t_readline *src)
+{
+	char	*env_buff;
+	int		len;
+	int		tmp;
+	int		i;
 
-// 	tok = malloc(sizeof(t_token));
-// 	ft_memset(tok, 0, sizeof(t_token));
+	len = 0;
+	tmp = src->now_pos;
+	if (token_case(move_char(src)) == DOLLAR)
+	{
+		i = 0;
+		env_buff = malloc (ft_env_len(src) + 1);
+		while (token_case(see_char(src)) == DOLLAR)//달러뒤에 달러인 경우는 무시
+			env_buff[i++] = move_char(src);
+		while (token_case(see_char(src)) == CHAR)
+			env_buff[i++] = move_char(src);
+		env_buff[i] = '\0';
+		len += ft_strlen(get_env(env_buff));
+		my_free((void **) &env_buff);
+	}
+	len++;
+	src->now_pos = tmp;
+	return (len);
+}
 
-// 	move_char(src);
-// 	while (token_case(see_char(src)) == DOLLAR)
-// 	{
-// 		env_buff[k] = move_char(src);
-// 		k++;
-// 	}
-// 	while (token_case(see_char(src)) == CHAR)
-// 	{
-// 		env_buff[k] = move_char(src);
-// 		k++;
-// 	}
-// 	env_buff[k] = '\0';
-// 	env_text = get_env(env_buff);//이거 환경변수 세팅해서 바꿔야되는데 일단 토큰부터 다 만들고 진행할 예정
-// 	my_free((void**)&env_buff);
-// 	if (k == 0) //달러 하나인 경우에는 $로 봐야해요
-// 		env_text = "$";
-// 	while (env_text && *env_text)
-// 		tok_buff[i++] = *env_text++;
-// 	tok_buff[i] = '\0';
-// 	i++;
-// 	tok->text = malloc(sizeof(char) * (i));
-// 	tok->text_len = i;
-// 	while (j < i)
-// 	{
-// 		tok->text[j] = tok_buff[j];
-// 		j++;
-// 	}
-// 	my_free((void**)&tok_buff);
-// 	tok->text_len = j;
-// 	tok->tok_type = ARGV_TOK;
-// 	return (tok);
-// }
+t_token	*create_dollar_tok(t_readline *src)
+{
+	t_token	*tok;
+	char	*env_text;
+	char	*tok_buff;
+	int		i;
+
+	i = 0;
+	tok = malloc(sizeof(t_token));
+	ft_memset(tok, 0, sizeof(t_token));
+	tok_buff = malloc(ft_dollar_len(src) + 1);
+	env_text = make_env_text(src);
+	while (env_text && *env_text)
+		tok_buff[i++] = *env_text++;
+	tok_buff[i] = '\0';
+	while (env_text && *env_text)
+		tok_buff[i++] = *env_text++;
+	tok_buff[i] = '\0';
+	i++;
+	tok->text = tok_buff;
+	tok->text_len = i;
+	tok->tok_type = ARGV_TOK;
+	return (tok);
+}
+
+int	char_tok_len(t_readline *src)
+{
+	int		len;
+	int		i;
+
+	len = 0;
+	i = src->now_pos;
+	while (token_case(see_char(src)) == CHAR)
+	{
+		move_char(src);
+		len++;
+	}
+	src->now_pos = i;
+	return (len);
+}
+
+t_token	*create_char_tok(t_readline *src)
+{
+	t_token	*tok;
+	int		len;
+	int		i;
+
+	i = 0;
+	tok = malloc(sizeof(t_token));
+	len = char_tok_len(src);
+	tok->text = malloc (len + 1);
+	tok->text_len = len;
+	while (token_case(see_char(src)) == CHAR)
+	{
+		tok->text[i] = move_char(src);
+		i++;
+	}
+	tok->text[i] = '\0';
+	if (see_char(src) != ENDOF)
+		move_char(src);
+	tok->tok_type = ARGV_TOK;
+	return (tok);
+}
 
 t_token	*create_token(t_readline *src)
 {
-	t_token	*tok;
-	char	*tok_buff;
-	char	*env_buff;
-	char	*env_text;
-	int		i;
-	int		j;
-	int		k;
-
-	tok_buff = malloc(sizeof(char) * 1000);
-	env_buff = malloc(sizeof(char) * 1000);
-	tok = malloc(sizeof(t_token));
-	i = 0;
-	j = 0;
-	k = 0;
-	if (!tok || !tok_buff)
-		return (NULL);
-	ft_memset(tok, 0, sizeof(t_token));
 	if (token_case(see_char(src)) == QUOTES)//작은 따옴표인 경우 쭉 토큰화를 하고
 		return (create_quotes(src));
 	else if (token_case(see_char(src)) == D_QUOTES)
@@ -366,67 +399,13 @@ t_token	*create_token(t_readline *src)
 	else if (token_case(see_char(src)) == PIPE)
 		return (create_pipe_tok(src));
 	else if (token_case(see_char(src)) == DOLLAR)
-	{
-		move_char(src);
-		while (token_case(see_char(src)) == DOLLAR)
-		{
-			env_buff[k] = move_char(src);
-			k++;
-		}
-		while (token_case(see_char(src)) == CHAR)
-		{
-			env_buff[k] = move_char(src);
-			k++;
-		}
-		env_buff[k] = '\0';
-		env_text = get_env(env_buff);//이거 환경변수 세팅해서 바꿔야되는데 일단 토큰부터 다 만들고 진행할 예정
-		my_free((void**)&env_buff);
-		if (k == 0) //달러 하나인 경우에는 $로 봐야해요
-			env_text = "$";
-		while (env_text && *env_text)
-			tok_buff[i++] = *env_text++;
-		tok_buff[i] = '\0';
-		i++;
-		// if (k != 0)
-		// 	move_char(src);
-		tok->text = malloc(sizeof(char) * (i));
-		tok->text_len = i;
-		while (j < i)
-		{
-			tok->text[j] = tok_buff[j];
-			j++;
-		}
-		my_free((void**)&tok_buff);
-		tok->text_len = j;
-		tok->tok_type = ARGV_TOK;
-		return (tok);
-	}
+		return (create_dollar_tok(src));
 	else if (token_case(see_char(src)) == DIRECT)
 		return (create_direct_tok(src));
 	else if (token_case(see_char(src)) == RE_DIRECT)
 		return (create_redirect_tok(src));
 	else if (token_case(see_char(src)) == CHAR)
-	{
-		while (token_case(see_char(src)) == CHAR && see_char(src) != ENDOF)
-		{
-			tok_buff[i] = move_char(src);
-			i++;
-		}
-		tok_buff[i] = '\0';
-		i++;
-		tok->text = malloc(sizeof(char) * (i));
-		tok->text_len = i;
-		while (j < i)
-		{
-			tok->text[j] = tok_buff[j];
-			j++;
-		}
-		my_free((void**)&tok_buff);
-		my_free((void**)&env_buff);
-		tok->text_len = j;
-		tok->tok_type = ARGV_TOK;
-		return (tok);
-	}
+		return (create_char_tok(src));
 	return (NULL);
 }
 
