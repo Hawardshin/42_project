38717d6f2d4b958b789ad9251bbaa0f2bc8bb260
wnddms12345 <wnddms12345@naver.com>
2022-11-16 19:17:27 +0900

    일단 달러랑 문자 빼고 다 함수화 완료

--- REAL_MINISHELL/token.c
+++ REAL_MINISHELL/token.c
@@ -6,7 +6,7 @@
 /*   By: joushin <joushin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 17:14:42 by joushin           #+#    #+#             */
-/*   Updated: 2022/11/16 18:47:09 by joushin          ###   ########.fr       */
+/*   Updated: 2022/11/16 19:16:59 by joushin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -226,6 +226,7 @@ t_token	*create_d_quotes(t_readline *src)
 
 	move_char(src);
 	tok = malloc(sizeof(t_token));
+	ft_memset(tok, 0, sizeof(t_token));
 	tok->text = make_double_tok(src);
 	if (token_case(see_char(src)) != D_QUOTES)
 		g_state.exit_code = 258;
@@ -236,6 +237,106 @@ t_token	*create_d_quotes(t_readline *src)
 	return (tok);
 }
 
+t_token	*create_space_tok(t_readline *src)
+{
+	t_token	*tok;
+
+	tok = malloc(sizeof(t_token));
+	ft_memset(tok, 0, sizeof(t_token));
+	while (token_case(see_char(src)) == SPACE_B && see_char(src) != ENDOF)
+		move_char(src);
+	tok->text = "SPACE";
+	tok->tok_type = SPACE_TOK;
+	return (tok);
+}
+
+t_token	*create_pipe_tok(t_readline *src)
+{
+	t_token	*tok;
+
+	tok = malloc(sizeof(t_token));
+	ft_memset(tok, 0, sizeof(t_token));
+	move_char(src);
+	tok->text = "PIPE'|'";
+	tok->tok_type = PIPE_TOK;
+	return (tok);
+}
+
+t_token	*create_direct_tok(t_readline *src)
+{
+	t_token	*tok;
+
+	tok = malloc(sizeof(t_token));
+	ft_memset(tok, 0, sizeof(t_token));
+	move_char(src);
+	if (token_case(see_char(src)) == DIRECT)
+	{
+		move_char(src);
+		tok->text = "<<";
+	}
+	else
+		tok->text = "<";
+	tok->tok_type = IO_TOK;
+	return (tok);
+}
+
+t_token	*create_redirect_tok(t_readline *src)
+{
+	t_token	*tok;
+
+	tok = malloc(sizeof(t_token));
+	ft_memset(tok, 0, sizeof(t_token));
+	move_char(src);
+	if (token_case(see_char(src)) == RE_DIRECT)
+	{
+		move_char(src);
+		tok->text = ">>";
+	}
+	else
+		tok->text = ">";
+	tok->tok_type = IO_TOK;
+	return (tok);
+}
+
+// t_token	*create_dollar_tok(t_readline *src)
+// {
+// 	t_token	*tok;
+
+// 	tok = malloc(sizeof(t_token));
+// 	ft_memset(tok, 0, sizeof(t_token));
+
+// 	move_char(src);
+// 	while (token_case(see_char(src)) == DOLLAR)
+// 	{
+// 		env_buff[k] = move_char(src);
+// 		k++;
+// 	}
+// 	while (token_case(see_char(src)) == CHAR)
+// 	{
+// 		env_buff[k] = move_char(src);
+// 		k++;
+// 	}
+// 	env_buff[k] = '\0';
+// 	env_text = get_env(env_buff);//이거 환경변수 세팅해서 바꿔야되는데 일단 토큰부터 다 만들고 진행할 예정
+// 	my_free((void**)&env_buff);
+// 	if (k == 0) //달러 하나인 경우에는 $로 봐야해요
+// 		env_text = "$";
+// 	while (env_text && *env_text)
+// 		tok_buff[i++] = *env_text++;
+// 	tok_buff[i] = '\0';
+// 	i++;
+// 	tok->text = malloc(sizeof(char) * (i));
+// 	tok->text_len = i;
+// 	while (j < i)
+// 	{
+// 		tok->text[j] = tok_buff[j];
+// 		j++;
+// 	}
+// 	my_free((void**)&tok_buff);
+// 	tok->text_len = j;
+// 	tok->tok_type = ARGV_TOK;
+// 	return (tok);
+// }
 
 t_token	*create_token(t_readline *src)
 {
@@ -261,26 +362,9 @@ t_token	*create_token(t_readline *src)
 	else if (token_case(see_char(src)) == D_QUOTES)
 		return (create_d_quotes(src));
 	else if (token_case(see_char(src)) == SPACE_B)
-	{
-		while (token_case(see_char(src)) == SPACE_B && see_char(src) != ENDOF)
-		{
-			move_char(src);
-		}
-		tok->text = "SPACE";
-		tok->tok_type =SPACE_TOK;
-		my_free((void**)&env_buff);
-		my_free((void**)&tok_buff);
-		return (tok);
-	}
+		return (create_space_tok(src));
 	else if (token_case(see_char(src)) == PIPE)
-	{
-		move_char(src);
-		tok->text = "PIPE'|'";
-		tok->tok_type = PIPE_TOK;
-		my_free((void**)&env_buff);
-		my_free((void**)&tok_buff);
-		return (tok);
-	}
+		return (create_pipe_tok(src));
 	else if (token_case(see_char(src)) == DOLLAR)
 	{
 		move_char(src);
@@ -296,8 +380,6 @@ t_token	*create_token(t_readline *src)
 		}
 		env_buff[k] = '\0';
 		env_text = get_env(env_buff);//이거 환경변수 세팅해서 바꿔야되는데 일단 토큰부터 다 만들고 진행할 예정
-		// printf("envtext:%s\n",env_text);
-		// my_free((void**)&env_text);
 		my_free((void**)&env_buff);
 		if (k == 0) //달러 하나인 경우에는 $로 봐야해요
 			env_text = "$";
@@ -320,35 +402,9 @@ t_token	*create_token(t_readline *src)
 		return (tok);
 	}
 	else if (token_case(see_char(src)) == DIRECT)
-	{
-		move_char(src);
-		if (token_case(see_char(src)) == DIRECT)
-		{
-			move_char(src);
-			tok->text = "<<";
-		}
-		else
-			tok->text = "<";
-		tok->tok_type = IO_TOK;
-		my_free((void**)&env_buff);
-		my_free((void**)&tok_buff);
-		return (tok);
-	}
+		return (create_direct_tok(src));
 	else if (token_case(see_char(src)) == RE_DIRECT)
-	{
-		move_char(src);
-		if (token_case(see_char(src)) == RE_DIRECT)
-		{
-			move_char(src);
-			tok->text = ">>";
-		}
-		else
-			tok->text = ">";
-		tok->tok_type = IO_TOK;
-		my_free((void**)&env_buff);
-		my_free((void**)&tok_buff);
-		return (tok);
-	}
+		return (create_redirect_tok(src));
 	else if (token_case(see_char(src)) == CHAR)
 	{
 		while (token_case(see_char(src)) == CHAR && see_char(src) != ENDOF)
@@ -371,14 +427,6 @@ t_token	*create_token(t_readline *src)
 		tok->tok_type = ARGV_TOK;
 		return (tok);
 	}
-	// else if (see_char(src) == EOF)
-	// {
-	// 	tok->text = "EOF";
-	// 	tok->tok_type = EOF_TOK;
-	// 	my_free((void**)&env_buff);
-	// 	my_free((void**)&tok_buff);
-	// 	return (tok);
-	// }
 	return (NULL);
 }
 
@@ -415,52 +463,6 @@ void	merge_two_tok(t_token *front, t_token *back)
 	back = NULL;
 }
 
-// void	delete_all_null_tok(t_main_token *tok) // 이건 쓸지 안 쓸지 모르겠습니다. 널 토큰같은 경우 어떻게 처리할지는 나중에 정할께용 아마 노드만들때 그냥 토큰 지워버리는게 더 합리적일듯?
-// {
-// 	t_token	*tmp;
-// 	t_token	*ntmp;
-
-// 	tmp = tok->start_token;
-// 	while (tmp->next != NULL)
-// 	{
-// 		if (tmp->tok_type == ARGV_TOK && tmp->text_len == 1)
-// 		{
-// 			if (tmp->bef==NULL)//시작 토큰이라면
-// 			{
-// 				ntmp = tmp->next;
-// 				my_free((void**)&tmp);
-// 				tmp = ntmp;
-// 				tmp->bef = NULL;
-// 				tok->start_token = tmp;
-// 			}
-// 			else
-// 			{
-// 				ntmp = tmp->next;
-// 				tmp->bef->next = tmp->next;
-// 				if (tmp->next != NULL)
-// 					tmp->next->bef = tmp->bef;
-// 				my_free((void**)&tmp);
-// 				tmp = ntmp;
-// 			}
-// 		}
-// 		else
-// 			tmp = tmp->next;
-// 	}
-// 	if (tmp->tok_type == ARGV_TOK && tmp->text_len == 1)
-// 	{
-// 		if (tmp->bef == NULL)//시작 토큰이라면
-// 		{
-// 			my_free((void**)&tmp);
-// 			tok->start_token = NULL;
-// 			tok->end_token = NULL;
-// 		}
-// 		else
-// 		{
-// 			tmp->bef->next = NULL;
-// 			my_free((void**)&tmp);
-// 		}
-// 	}
-// }
 void	merge_argv_tok(t_main_token *tok)
 {
 	t_token	*tmp;
